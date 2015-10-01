28ee8f51b136de38fcba85b67aa4df7607033caf
Stéphane Wouters <stephane.wouters@etu.umontpellier.fr>
2015-10-01 17:37:38 +0200

    Refactoring

--- server/main.go
+++ server/main.go
@@ -1,28 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-)
-
-// Main chanel
-var Ch chan int
-
-// Logger d'erreur principal
-var ErrLogger = log.New(os.Stderr, "Erreur: ", 0)
-
-func startHTTPServer(port int) {
-	fmt.Printf("Start http server on port %d...\n", port)
-	http.Handle("/", http.FileServer(http.Dir("../client")))
-	Ch <- 1
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
-	if err != nil {
-		ErrLogger.Println("Erreur à la création du serveur HTTP : ", err.Error())
-	}
-	Ch <- 1
-}
+import "fmt"
 
 func main() {
 
