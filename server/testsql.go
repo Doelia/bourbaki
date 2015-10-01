0eef3f27429fc8fb546da9725c9094b7454dc7b0
Guillemette Marlene <marlene.guillemette@etu.umontpellier.fr>
2015-10-01 18:33:48 +0200

    Exemple du fonctionnement de Bolt

--- server/testsql.go
+++ server/testsql.go
@@ -11,4 +11,25 @@ func Testsql() {
 		fmt.Print("Erreur")
 	}
 	defer db.Close()
+
+	tx, err2 := db.Begin(true)
+	if err2 != nil {
+		fmt.Println("Erreur création db")
+	}
+	defer tx.Rollback()
+
+	b, err3 := tx.CreateBucket([]byte("MyBucket"))
+	if err3 != nil {
+		fmt.Println("Erreur création bucket")
+	}
+
+	err4 := b.Put([]byte("testclé"), []byte("testvaleur"))
+	if err4 != nil {
+		fmt.Println("Erreur put")
+	}
+
+	br := tx.Bucket([]byte("MyBucket"))
+	v := br.Get([]byte("testclé"))
+	fmt.Printf("Le résultat serait : %s\n", v)
+
 }
