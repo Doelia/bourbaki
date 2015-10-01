493749536aff675a937a2a7a1c00b904f545f757
Guillemette Marlene <marlene.guillemette@etu.umontpellier.fr>
2015-10-01 17:44:01 +0200

    Test mise en place BD

---
+++ server/testsql.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/mattn/go-sqlite3"
+)
+
+var DB_DRIVER string
+
+func Testsql() {
+	sql.Register(DB_DRIVER, &sqlite3.SQLiteDriver{})
+	database, err := sql.Open(DB_DRIVER, "mysqlite_3")
+	if err != nil {
+		fmt.Println("Failed to create the handle")
+	}
+	if err2 := database.Ping(); err2 != nil {
+		fmt.Println("Failed to keep connection alive")
+	}
+	fmt.Println("Hello world")
+}
