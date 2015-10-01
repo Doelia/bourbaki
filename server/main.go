1fd7078dca75f412eff678c62df72b5bbc3fb1bd
Stéphane Wouters <stephane.wouters@etu.umontpellier.fr>
2015-10-01 16:57:20 +0200

    Ajout serveur HTTP static

--- server/main.go
+++ server/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+// Main chanel
+var Ch chan int
+
+// Logger d'erreur principal
+var ErrLogger = log.New(os.Stderr, "Erreur: ", 0)
+
+func startHTTPServer(port int) {
+	fmt.Printf("Start htpp server on port %d...\n", port)
+	http.Handle("/", http.FileServer(http.Dir("../client")))
+	Ch <- 1
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		ErrLogger.Println("Erreur à la création du serveur HTTP : ", err.Error())
+	}
+	Ch <- 1
+}
 
 func main() {
-	fmt.Println("Hello world")
+
+	Ch = make(chan int, 1)
+
+	fmt.Println("=== BOURBAKI SERVEUR ===")
+
+	// Création serveur HTTP
+	go startHTTPServer(2000)
+	<-Ch
+
+	fmt.Println("Next")
+
+	<-Ch
 }
